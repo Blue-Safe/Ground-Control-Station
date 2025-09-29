25bd5d2395d7ff160123134fa92bf974f8142a96
Bluesafe <isaiahsmith1776@gmail.com>
2025-09-29 20:57:34 +0000

    Worked on boilerplate code for PyQt

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
-import serial
+import serial, sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QLabel
 
 # Test to make sure pyserial works
 print("pyserial OK:", serial.__version__)
 
 
+# ---------------  Gui  -------------------#
+
+app = QApplication(sys.argv)
+
+# The Main window object.
+
+class Main(QMainWindow):
+
+    # Main window constructor. Uses parents
+    # __init__ method, sets the title, and
+    # establishes formatting and static widgets
+    # for the application.
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Ground Control Station")
+        central = QWidget(self)
+        self.setCentralWidget(central)
+        layout = QVBoxLayout(central)
+
+        lable = QLabel("Hello world")
+        layout.addWidget(lable)
+
+
+
+
+if __name__ == "__main__":
+    win = Main()
+    win.resize(900,600)
+    win.show()
+    sys.exit(app.exec_())
